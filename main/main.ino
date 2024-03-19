8d881b1f16827c85bc1d95bc50e43c20c0e2d54f
toikkah2 <toikkah2@aalto.fi>
2024-03-19 21:53:17 +0200

    new changes

--- main/main.ino
+++ main/main.ino
@@ -8,6 +8,23 @@ static TFT_eSPI* tft = nullptr;
 static Buzzer* buzzer = nullptr;
 static TinyGPSPlus* gps = nullptr;
 static Adafruit_DRV2605* drv = nullptr;
+HardwareSerial *GNSS = NULL;
+
+TFT_eSprite *sprite_Time = nullptr;
+TFT_eSprite *sprite_Location = nullptr;
+TFT_eSprite *sprite_Coordinate = nullptr;
+
+void pressed(){
+    drv->setWaveform(0, 75);
+    drv->setWaveform(1, 0);
+    drv->go();
+}
+
+void released(){
+    drv->setWaveform(0, 75);
+    drv->setWaveform(1, 0);
+    drv->go();
+}
 
 void clearScreen(){
     tft->fillScreen(TFT_BLACK);
@@ -56,33 +73,100 @@ void setup(){
 
     tft = ttgo->tft;
     tft->setTextFont(2);
-    
-    ttgo->trunOnGPS();
-    ttgo->gps_begin();
-    gps =  ttgo->gps;
 
     drv = ttgo->drv;
     ttgo->enableDrv2650();
 
+    ttgo->trunOnGPS();
+    ttgo->gps_begin();
+    gps =  ttgo->gps;
+    GNSS = ttgo->hwSerial;
+
+    if(!Quectel_L76X_Probe()) {
+        tft->setCursor(0, 0);
+        tft->setTextColor(TFT_RED);
+        tft->println("GNSS Probe failed!");
+        while (1);
+    }
+
     tft->fillScreen(TFT_RED);
     tft->setCursor(0, 0);
     tft->println("Play Audio!");
+    // eSpTime->setTextColor(TFT_GREEN, TFT_BLACK);
+
+    sprite_Time = new TFT_eSprite(tft);
+    sprite_Location = new TFT_eSprite(tft);
+    sprite_Coordinate = new TFT_eSprite(tft);
+
+    sprite_Time->createSprite(240, 48);
+    sprite_Time->setTextFont(2);
+    sprite_Location->createSprite(240, 48);
+    sprite_Location->setTextFont(2);
+    sprite_Coordinate->createSprite(240, 48);
+    sprite_Coordinate->setTextFont(2);
+
+    ttgo->button->setPressedHandler(pressed);
+    ttgo->button->setReleasedHandler(released);
 
     clearScreen();
 }
 
 char buf[128];
+char gpsBuf[128];
+char gpsTimeBuf[128];
 
 void loop(){
-    int16_t x, y;
-    if (ttgo->getTouch(x, y)) {
-        sprintf(buf, "x:%03d  y:%03d", x, y);
-        clearScreen();
-        ttgo->tft->drawString(buf, x, y);
-        drv->setWaveform(0, 75);  // play effect
-        drv->setWaveform(1, 0);       // end waveform
-        drv->go();
-        delay(50);
+    while(GNSS->available()) {
+        int r = GNSS->read();
+        Serial.write(r);
+        gps->encode(r);
     }
-    delay(100);
+    
+    bool doClearScreen = false;
+    int16_t x, y;
+    doClearScreen = doClearScreen || (bool)ttgo->getTouch(x, y);
+    doClearScreen = doClearScreen || (bool)gps->location.isUpdated();
+
+    doClearScreen = false;
+    if(doClearScreen){
+        clearScreen();
+    }
+
+    if (gps->location.isUpdated()) {
+        sprintf(gpsBuf, "Location: lat %.6f long %.6f", gps->location.lat(), gps->location.lng());
+        
+        sprite_Location->fillSprite(TFT_BLACK);
+        sprite_Location->setTextColor(TFT_GREEN);
+        sprite_Location->setCursor(0, 0);
+        sprite_Location->print(gpsBuf);
+        sprite_Location->pushSprite(0, 0);
+        // ttgo->tft->drawString(gpsBuf, 10, 80);
+    }
+
+    if (gps->time.isUpdated()) {
+        sprintf(gpsTimeBuf, "Time: %02d:%02d:%02d", gps->time.hour(), gps->time.minute(), gps->time.second());
+
+        sprite_Time->fillSprite(TFT_BLACK);
+        sprite_Time->setTextColor(TFT_RED);
+        sprite_Time->setCursor(0, 0);
+        sprite_Time->print(gpsTimeBuf);
+        sprite_Time->pushSprite(0, 43);
+        // ttgo->tft->drawString(gpsTimeBuf, 10, 40);
+    }
+
+    if (ttgo->getTouch(x, y)) {
+        sprintf(buf, "x=%03d  y=%03d", x, y);
+
+        sprite_Coordinate->fillSprite(TFT_BLACK);
+        sprite_Coordinate->setTextColor(TFT_YELLOW);
+        sprite_Coordinate->setCursor(0, 0);
+        sprite_Coordinate->print(buf);
+        sprite_Coordinate->pushSprite(240/3, 240 - 43);
+
+        // ttgo->tft->drawString(buf, x, y);
+        drv->setWaveform(0, 75);
+        drv->setWaveform(1, 0);
+        drv->go();
+    }
+    // delay(5);
 }
